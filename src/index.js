4957f090d0763b52b74aaff48c8fd8c55a441ab8
VoitV <v.voitenko123@gmail.com>
2022-05-17 09:37:17 +0300

    (#5) use handlebar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import path from "path";
 import express from "express";
+import hbs from "hbs";
+import { engine } from "express-handlebars";
+
+const __dirname = path.resolve();
+const PORT = 3000;
+const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.static(__dirname + "/public"));
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.get("/", (req, res) => {
+  res.render("pages/home-page", {
+    title: "Eshop22",
+  });
+});
+
+app.use(function (req, res, next) {
+  res.statusCode = 404;
+  res.render("pages/not-found-page", {
+    title: "Sorry, page not found",
+    url: req.url,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("created server, port:", PORT);
+});
